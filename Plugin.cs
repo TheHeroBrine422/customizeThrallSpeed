9b63dc1a20cd320d56ee33cd46fd18a58e80510f
Hero <caleb@calebgj.io>
2022-06-12 18:12:56 -0500

    pregit code

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using Wetstone.API;
+
+namespace customizeThrallSpeed
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("xyz.molenzwiebel.wetstone")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            if (VWorld.IsServer)
+            {
+                Log.LogInfo("I am on the server!");
+            }
+
+            if (VWorld.IsClient)
+            {
+                Log.LogInfo("I am on the client!");
+            }
+        }
+    }
+}
